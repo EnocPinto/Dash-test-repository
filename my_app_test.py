c006eca8a3e4e6e385ba4b877806c192a15821df
EnocPinto <enoc.pinto@gmail.com>
2023-06-25 22:50:28 +0200

    Update my_app_test.py

--- my_app_test.py
+++ my_app_test.py
@@ -5,7 +5,6 @@ Created on Sat Jun 17 12:01:07 2023
 @author: noco_
 """
 import dash
-import dash_bootstrap_components as dbc
 import dash_html_components as html
 import dash_core_components as dcc
 import pandas as pd
